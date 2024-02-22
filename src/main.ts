0a42fa02449942eb53257f125fd0a10569c9d486
altbdoor <lancersupraskyline@gmail.com>
2024-02-22 14:15:56 +0800

    add optimization config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import { ILocationProvider, module, route } from "angular";
+import { ICompileProvider, ILocationProvider, module, route } from "angular";
 import ngRoute from "angular-route";
-import { componentName as HelloComponentName, HelloComponent } from "./components/hello/hello.component";
+import { HelloComponent, componentName as HelloComponentName } from "./components/hello/hello.component";
 import "./style.css";
 
-module("SampleApp", [ngRoute])
+module("AngularJSApp", [ngRoute])
     .component(HelloComponentName, new HelloComponent())
-    .config(($routeProvider: route.IRouteProvider, $locationProvider: ILocationProvider) => {
-        $locationProvider.hashPrefix("");
-        $routeProvider.when("/home", { template: "<hello></hello>" }).otherwise("/home");
-    })
+    .config([
+        "$compileProvider",
+        ($compileProvider: ICompileProvider) => {
+            // $compileProvider.debugInfoEnabled(false);
+            $compileProvider.commentDirectivesEnabled(false);
+            $compileProvider.cssClassDirectivesEnabled(false);
+        },
+    ])
+    .config([
+        "$routeProvider",
+        "$locationProvider",
+        ($routeProvider: route.IRouteProvider, $locationProvider: ILocationProvider) => {
+            $locationProvider.hashPrefix("");
+            $routeProvider.when("/home", { template: "<hello></hello>" }).otherwise("/home");
+        },
+    ])
     .run(() => {});
